849918b9c46fe4904c87a01ceb7d4f925a1809dd
amni2000 <146669169+Toleas03@users.noreply.github.com>
2024-03-21 18:03:20 +0000

    Basic functionality done
    Todo: add groups in saved and fix search

--- popup.js
+++ popup.js
@@ -1,27 +1,111 @@
 const tabs = await chrome.tabs.query({});
 
-function loadCurrentTabs() {
+function tabExists(tabId) {
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get(null, (result) => {
+            resolve(result.hasOwnProperty(tabId.toString()));
+        });
+    });
+}
+
+function removeTab(tabId) {
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.remove(tabId.toString(), () => {
+            resolve();
+        });
+    });
+}
+
+async function loadCurrentTabs() {
     const template = document.getElementById('tab-template');
     const elements = new Set();
     for (const tab of tabs) {
         const element = template.content.firstElementChild.cloneNode(true);
 
         const title = tab.title.split('-')[0].trim();
-        const pathname = new URL(tab.url)
+        const pathname = tab.url;
 
-        var titleEl = element.querySelector('.tab-title')
-        var url = element.querySelector('.tab-url')
+        var titleEl = element.querySelector('.tab-title');
+        var url = element.querySelector('.tab-url');
+        var save = element.querySelector('.tab-save')
+
         titleEl.textContent = title;
         url.textContent = pathname;
+
         url.addEventListener('click', () => {
             chrome.tabs.create({ url: tab.url });
         });
 
+        const saved = await tabExists(tab.id);
+
+        console.log(saved);
+        if (saved) {
+            save.innerHTML = 'Saved';
+            save.classList.add('saved');
+        }
+        else {
+            save.innerHTML = 'Save';
+            save.classList.remove('saved');
+        }
+
+        save.addEventListener('click', () => {
+            if (saved) {
+                console.log('Tab already exists');
+                return;
+            }
+            else {
+                const tabData = {
+                    title: title,
+                    url: tab.url
+                };
+
+                chrome.storage.local.set({ [tab.id]: tabData }, () => {
+                    console.log('Tab saved');
+                });
+            }
+            loadCurrentTabs();
+        });
         elements.add(element);
     }
     var tabcontents = document.getElementById('tab-content')
     tabcontents.innerHTML = '';
     tabcontents.append(...elements);
+}
+
+async function loadSavedTabs() {
+    // Call function for button 2
+    document.getElementById('tab-content').innerHTML = '';
+    chrome.storage.local.get(null, (items) => {
+        const elements = new Set();
+        const template = document.getElementById('tab-template');
+        for (const [key, tab] of Object.entries(items)) {
+            console.log(`Tab ID: ${key}, Title: ${tab.title}, URL: ${tab.url}`);
+            const element = template.content.firstElementChild.cloneNode(true);
+
+            const title = tab.title.split('-')[0].trim();
+            const pathname = tab.url
+
+            var titleEl = element.querySelector('.tab-title')
+            var url = element.querySelector('.tab-url')
+            var save = element.querySelector('.tab-save')
+
+            titleEl.textContent = title;
+            url.textContent = pathname;
+            url.addEventListener('click', () => {
+                chrome.tabs.create({ url: tab.url });
+            });
+            elements.add(element);
+
+            save.innerHTML = 'Remove';
+            save.addEventListener('click', () => {
+                removeTab(key);
+                loadSavedTabs();
+            });
+        }
+        var tabcontents = document.getElementById('tab-content')
+        tabcontents.innerHTML = '';
+        tabcontents.append(...elements);
+    });
 }
 
 loadCurrentTabs();
@@ -36,8 +120,7 @@ buttons.forEach(button => {
             // Call function for button 1
             loadCurrentTabs();
         } else if (button.id === 'savedTabBtn') {
-            // Call function for button 2
-            document.getElementById('tab-content').innerHTML = '';
+            loadSavedTabs();
         }
         // Add more else if statements for additional buttons
 
